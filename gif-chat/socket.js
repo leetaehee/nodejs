304704b947a13576df4a11115af27c791d38cc35
leetaehee <lastride25@naver.com>
2021-03-05 18:09:29 +0900

    소켓통신을 이용한 채팅 구현 완료

--- gif-chat/socket.js
+++ gif-chat/socket.js
@@ -1,6 +1,7 @@
 const SocketIO = require('socket.io');
 const axios = require('axios');
 const cookieParser = require('cookie-parser');
+const cookie = require('cookie-signature');
 
 module.exports = (server, app, sessionMiddleware) => {
     const io = SocketIO(server, { path: '/socket.io' });
@@ -10,7 +11,7 @@ module.exports = (server, app, sessionMiddleware) => {
 
     io.use((socket, next) => {
         cookieParser(process.env.COOKIE_SECRET)(socket.request, socket.request.res, next);
-        sessionMiddleware(socket.request, socket.request.res, next);
+        sessionMiddleware(socket.request, socket.request.res , next);
     });
 
     room.on('connection', (socket) => {
@@ -23,6 +24,7 @@ module.exports = (server, app, sessionMiddleware) => {
     chat.on('connection', (socket) => {
         console.log('chat 네임스페이스 접속');
         const req = socket.request;
+
         const { headers : { referer } } = req;
         const roomId = referer
             .split('/')[referer.split('/').length-1]
@@ -40,18 +42,18 @@ module.exports = (server, app, sessionMiddleware) => {
             const userCount = currentRoom ? currentRoom.length : 0;
             if (userCount === 0) { // 접속자 수가 0명이면 방 삭제 
                 const signedCookie = req.signedCookies['connect.sid'];
-                const connectSID = cookies.sign(signedCookie, process.env.COOKIE_SECRET);
+                const connectSID = cookie.sign(signedCookie, process.env.COOKIE_SECRET);
                 axios.delete(`http://localhost:8005/room/${roomId}`, {
                     headers: {
                         Cookie: `connect.sid=s%3A${connectSID}`,
-                    }, 
+                    },
                 })
-                    .then(() => {
-                        console.log('방 제거 요청 성공');
-                    })
-                    .catch((error) => {
-                        console.error(error);
-                    });
+                .then(() => {
+                    console.log('방 제거 요청 성공');
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
             } else {
                 socket.to(roomId).emit('exit', {
                     user: 'system',
