8b85886830e06066da97dfd6396d20bc94a023ac
leetaehee <lastride25@naver.com>
2021-03-08 21:59:46 +0900

    node-cli 세번째

--- node-cli/index.js
+++ node-cli/index.js
@@ -6,13 +6,19 @@ const r1 = readline.createInterface({
     output: process.stdout,
 });
 
-r1.question('예제가 재미있습니까? (y/n)', (answer) => {
+console.clear();
+const answerCallback = (answer) => {
     if (answer === 'y') {
-        console.log('감사합니다!');
+        console.log('감사합니다.!');
+        r1.close();
     } else if (answer === 'n') {
-        console.log('죄송합니다!');
+        console.log('죄송합니다.!');
+        r1.close();
     } else {
+        console.clear();
         console.log('y 또는 n만 입력하세요.');
+        r1.question('예제가 재미있습니까? (y/n) ', answerCallback);
     }
-    r1.close();
-});
+};
+
+r1.question('예제가 재미있습니까? (y/n) ', answerCallback);
