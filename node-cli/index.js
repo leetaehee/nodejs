223adf87feebe302f4833a545ca77a89c71df90b
leetaehee <lastride25@naver.com>
2021-03-08 21:48:21 +0900

    node-cli 두번째

--- node-cli/index.js
+++ node-cli/index.js
@@ -1,2 +1,18 @@
 #!/usr/bin/env node
-console.log('Hello CLI', process.argv);
+const readline = require('readline');
+
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+r1.question('예제가 재미있습니까? (y/n)', (answer) => {
+    if (answer === 'y') {
+        console.log('감사합니다!');
+    } else if (answer === 'n') {
+        console.log('죄송합니다!');
+    } else {
+        console.log('y 또는 n만 입력하세요.');
+    }
+    r1.close();
+});
